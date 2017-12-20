51c9a62158aa6fc46185ab1530cf000fb8e06eda
Howard Huang <ccool2486@gmail.com>
2017-12-20 18:37:54 +0800

    Better Linter: Standard

--- es6/test.js
+++ es6/test.js
@@ -1,16 +1,16 @@
-'use strict'
+"use strict";
 
 /** es6新增的變數指派方式 */
 const sentences = [
-  { subject: 'Javascript', verb: 'is', object: 'great' },
-  { subject: 'Elephants', verb: 'are', object: 'large' },
+  { subject: "Javascript", verb: "is", object: "great" },
+  { subject: "Elephants", verb: "are", object: "large" }
 ];
 
 /** log出所有的引數 */
 function say({ subject, verb, object }) {
-  console.log(`${subject} ${verb} ${object}`)
+  console.log(`${subject} ${verb} ${object}`);
 }
 
-for(let s of sentences){
-    say(s); 
+for (let s of sentences) {
+  say(s);
 }
