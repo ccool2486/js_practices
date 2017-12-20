de8320cd5a29ee60c2a3bca1cf6f7b4fd914b2a7
Howard Huang <ccool2486@gmail.com>
2017-12-20 10:34:36 +0800

    gulpfile comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 gulp.task('default', function(){
-    // Gulp tasks
-
+    
+    //node轉換
     gulp.src("es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest('dist'));
-    
+
+    //browser轉換
     gulp.src("public/es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest('public/dist'));
