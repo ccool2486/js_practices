e76ee28710081d274e7dc3e91fd76f1031b18c0b
Howard Huang <ccool2486@gmail.com>
2017-12-20 16:44:44 +0800

    jsdoc comment testing

--- es6/test.js
+++ es6/test.js
@@ -1,10 +1,12 @@
 'use strict'
 
+/** es6新增的變數指派方式 */
 const sentences = [
   { subject: 'Javascript', verb: 'is', object: 'great' },
   { subject: 'Elephants', verb: 'are', object: 'large' },
 ];
 
+/** log出所有的引數 */
 function say({ subject, verb, object }) {
   console.log(`${subject} ${verb} ${object}`)
 }
