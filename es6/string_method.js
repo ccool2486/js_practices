adeed3896f24ee3537282fd307215ec97d2af52d
Howard Huang <ccool2486@gmail.com>
2017-12-22 15:16:15 +0800

    string method comments

--- es6/string_method.js
+++ es6/string_method.js
@@ -14,6 +14,7 @@ var myName = "Oleg Shalygin";
 // 使用方式： myName.charAt(index)
 var letter = myName.charAt(6); // h
 
+// 某個字串在字串中的第一個位置，出現負值，就是沒有出現
 // String.prototype.indexOf()
 var fullName = "Oleg Shalygin";
 // 使用方式： fullName.indexOf(searchTerm, startingIndex)
@@ -23,7 +24,7 @@ index = fullName.indexOf("l"); // 1
 index = fullName.indexOf("l", 4); // 8
 index = fullName.indexOf("Girlfriend"); // -1 
 
-
+// 某個字串在字串中的最後一個位置，出現負值，就是沒有出現
 // String.prototype.lastIndexOf()
 var storyMode =
   "Once upon a time, there was a magical foobar that was controlling the universe...";
@@ -32,33 +33,39 @@ console.log(storyMode.lastIndexOf(".")); // 80
 console.log(storyMode.lastIndexOf("!")); // -1
 console.log(storyMode.lastIndexOf("foobar")); // 38
 
+// 跟indexof不同，這個吃正規表示式
 // String.prototype.match()
 var someString =
   "Hello there, my name is aararand and I am a magical foobarus creature";
 var resultsArray = someString.match(/a{2}/); 
 // resultsArray = ["aa", index: 24, input: "Hello there, my name is aararand and I am a magical foobarus creature"]
-var someOtherResultsArray = someString.match(/b{2}/);
+var someOtherResultsArray = someString.match(/b{2}/); 
 // someOtherResultsArray = null
 
+// 把某個條件的字串換成別的字串
 // String.prototype.replace()
 var someString =
   "Hello there, my name is aararand and I am a magical foobarus creature";
 var newString = someString.replace(/a{2}/, "lol");
 // newString = "Hello there, my name is lolrarand and I am a magical foobarus creature"
 
+// 刪除某個位置後的字元
 // String.prototype.slice()
 var story =
   "Foobarus is a magical unicorn with an ID of 21313 which flies higher than all other unicorns.  Unicorns fly?  Regardless!";
 var previewStory = story.slice(0, 40); // Foobarus is a magical unicorn with an ID
 
+// 用字串當做條件，把字串拆開。會回傳陣列，每個值都是拆開後的字串。
 // String.prototype.split()
 var story =
-  "Foobarus is a magical unicorn with an ID of 21313 which flies higher than all other unicorns.  Unicorns fly?  Regardless!";
+  "Foobarus is a magical unicorn with an ID of 21313 which flies higher than all other unicorns.   Unicorns fly?  Regardless!";
 var previewStory = story.split(".");
 console.log(previewStory[0]); // Foobarus is a magical unicorn with an ID of 21313 which flies higher than all other unicorns
 console.log(previewStory[1]); // Unicorns fly?  Regardless!
 
+// 擷取字串中一定數量的字元
 // String.prototype.substring()
+//
 // 注:
 // substring 方法的第二个参数代表截取停止位置的 index（不包含最后这个值）
 // 而 substr 的第二份参数则代表截取长度
@@ -78,14 +85,17 @@ var allUpperCase = story.toUpperCase();
 var allLowerCase = story.toLowerCase();
 var foobarus = allUpperCase.match(/FOOBARUS/); // ["FOOBARUS", index: 0, input: "FOOBARUS IS A MAGICAL UNICORN WITH AN ID OF 21313 …N ALL OTHER UNICORNS.  UNICORNS FLY?  REGARDLESS!]
 
+// 把開頭與結尾的空白清掉
 // String.prototype.trim()
 var fullName = "  Oleg Shalygin       ";
 var trimmedFullName = fullName.trim(); // Oleg Shalygin
 
+// 把值加在字串後面
 // String.prototype.concat()
 var firstName = "Oleg";
 var lastName = "Shalygin";
 var ID = 12321312;
 // 使用方式： firstName.concat(string2, string 3, string 3, ...);
+// 可以吃很多個arg，每個都會加在後面
 var fullIdentification = firstName.concat(lastName, ":", ID);
 console.log(fullIdentification); // OlegShalygin:12321312
