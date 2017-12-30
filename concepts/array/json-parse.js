30719574190456a4b2262272af6aa2d9e3ac6b43
Howard Huang <ccool2486@gmail.com>
2017-12-30 10:01:11 +0800

    JSON.parse的使用方法

---
+++ concepts/array/json-parse.js
@@ -0,0 +1,65 @@
+// JSON.parse() 可以把一個字串形態的JSON轉換成陣列供程式使用
+
+// 這裡是一個對象，不是一個JSON
+var colorsJson = {
+  "colors": [
+    {
+      "color": "black",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,255,1],
+        "hex": "#000"
+      }
+    },
+    {
+      "color": "white",
+      "category": "value",
+      "code": {
+        "rgba": [0,0,0,1],
+        "hex": "#FFF"
+      }
+    },
+    {
+      "color": "red",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,0,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "blue",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [0,0,255,1],
+        "hex": "#00F"
+      }
+    },
+    {
+      "color": "yellow",
+      "category": "hue",
+      "type": "primary",
+      "code": {
+        "rgba": [255,255,0,1],
+        "hex": "#FF0"
+      }
+    },
+    {
+      "color": "green",
+      "category": "hue",
+      "type": "secondary",
+      "code": {
+        "rgba": [0,255,0,1],
+        "hex": "#0F0"
+      }
+    },
+  ]
+} 
+
+var newData = JSON.stringify(colorsJson) // 可以用stringify把對象轉成字串
+var colors = JSON.parse(newData) // 可以用JSON.parse把字串轉成「陣列」
+
+console.log(colors.colors[0].code.hex) // #000
