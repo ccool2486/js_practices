b553ba8f471dd1dc3cc3279e4b22608137463f89
Howard Huang <ccool2486@gmail.com>
2018-01-02 17:54:37 +0800

    整理

---
+++ concepts/_sample_data/animals_array.js
@@ -0,0 +1,7 @@
+var animals = [
+  {name : 'Fluffykins', species : 'rabbit'},
+  {name : 'Caro', species : 'dog'},
+  {name : 'Hamilton', species : 'fish'},
+  {name : 'Harold', species : 'cat'},
+  {name : 'Jimmy', species : 'fish'}
+]
