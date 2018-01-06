5d645093773b103c2fd8ef50a90f0cd5a59a16de
Howard Huang <ccool2486@gmail.com>
2018-01-06 10:25:36 +0800

    整理

--- concepts/functional-programming/understand-functional-programming.js
+++ concepts/functional-programming/understand-functional-programming.js
@@ -3,11 +3,12 @@
 // 1. 幾乎所有的function都會回傳值： 常見的型態是陣列
 // 2. 因為都會回傳，所以串接function起來，可以方便處理資料
 
-var names = ['chris', 'sean', 'raine'] 
-
 // 把首字變成大寫... 這是一般的寫法
+// 範例１： 把首字變成大寫
+var names = ['chris', 'sean', 'raine']
+
 var namesToUppercase = []
-for (let i=0; i < names.length; i++) {
+for (let i = 0; i < names.length; i++) {
   let firstLetter = names[i][0]
   let restLetters = names[i].slice(1)
   namesToUppercase[i] = firstLetter.toUpperCase() + restLetters
@@ -18,7 +19,7 @@ namesToUppercase.join(' ,') // Chris ,Sean ,Raine
 // 這是functional的寫法，使用for
 function capitalizeNamesFor (inputNames) {
   let outputNames = []
-  for (let i=0; i < inputNames.length; i++) {
+  for (let i = 0; i < inputNames.length; i++) {
     let firstLetter = inputNames[i][0]
     let restLetters = inputNames[i].slice(1)
     outputNames[i] = firstLetter.toUpperCase() + restLetters
@@ -29,12 +30,12 @@ function capitalizeNamesFor (inputNames) {
 capitalizeNamesFor(names).join(' ,') // Chris ,Sean ,Raine
 
 // 這是functional的寫法，使用map
-// 我們可以看到他裡面也近一步使用了函式去回傳值
+// 我們可以看到他裡面也進一步使用了函式去回傳值
 var capitalNamesMap = names.map(
-  function(elem){
+  function (elem) {
     let firstLetter = elem[0]
     let restLetters = elem.slice(1)
     return firstLetter.toUpperCase() + restLetters
   }
 )
-console.log(capitalNamesMap.join(' ,')) // ​​​​​Chris ,Sean ,Raine​​​​​
+console.log(capitalNamesMap.join(' ,')) // Chris ,Sean ,Raine
