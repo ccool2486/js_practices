9405024d2d452fcd23117425a19643196ae4be6c
Howard Huang <ccool2486@gmail.com>
2018-01-07 10:38:09 +0800

    subclass

--- concepts/classes/contructor-functions.js
+++ concepts/classes/contructor-functions.js
@@ -18,8 +18,33 @@ User.prototype.getEmailAddress = function () {
 
 // 下面我們可以看到所有繼承的對象都會有這個屬性...
 // [重要！] emailDomain這個屬性，以及getEmailAddress方法，
-// 不是在這些對象裡面，而是在obj.prototype裡面
+// 不是在這些對象裡面，而是在obj.prototype裡面，因為JS是利用prototype概念來達成繼承概念的
 user1.emailDomain // @facebook.com
 user2.emailDomain // @facebook.com
 user1.getEmailAddress() // HowardHuang@facebook.com
 user2.getEmailAddress() //​​​​​ CathyWu@facebook.com​​​​​
+
+// subclass的做法...
+// 以下定義一個好人的類別，讓他繼承User類別
+function NicePerson (firstName, lastName, age, gender) {
+  this.firstName = firstName
+  this.lastName = lastName
+  this.age = age
+  this.gender = gender
+  this.attitude = 'Positive'
+  this.face = 'Smily'
+}
+
+NicePerson.prototype = User
+NicePerson.prototype.getAttitue = function () {
+  return this.attitude
+}
+var angie = new NicePerson('Angie', 'Wu', 35, 'Female')
+
+// 原型定義，以下看到會複寫原本的函式與屬性
+NicePerson.prototype.getEmailAddress = function () {
+  return 'nice' + this.firstName + this.lastName + this.emailDomain
+}
+NicePerson.prototype.emailDomain = '@nicefacebook.com'
+
+angie.getEmailAddress() // niceAngieWu@nicefacebook.com​​​​​
