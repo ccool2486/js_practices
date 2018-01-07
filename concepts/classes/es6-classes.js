5847d6c4241a60e0abf33412cec0e846295c903d
Howard Huang <ccool2486@gmail.com>
2018-01-07 10:38:32 +0800

    整理

--- concepts/classes/es6-classes.js
+++ concepts/classes/es6-classes.js
@@ -3,14 +3,14 @@
 
 // 創造一個class
 class Person {
-    constructor(firstName, lastName) {
-        this.firstName = firstName
-        this.lastName = lastName
-    }
-    
-    greet () {
-        console.log ( `Hi! ${this.firstName} ${this.lastName}` )
-    }
+  constructor (firstName, lastName) {
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+
+  greet () {
+    console.log(`Hi! ${this.firstName} ${this.lastName}`)
+  }
 }
 
 // 讓john是Person類別的一個實例
@@ -23,16 +23,16 @@ friend.greet() // Hi! Howard Huang
 
 // 讓Dancer類別是一個繼承Person的類別
 class Dancer extends Person {
-    constructor (firstName, lastName) {
-        super (firstName, lastName) // 繼承Person的建造方式
-    }
-    
-    dance () {
-        console.log(`Let's dance with ${this.firstName} ${this.lastName}`)
-    }
+  constructor (firstName, lastName) {
+    super(firstName, lastName) // 繼承Person的建造方式
+  }
+
+  dance () {
+    console.log(`Let's dance with ${this.firstName} ${this.lastName}`)
+  }
 }
 
 // 讓Cathy是Dancer類別的一個實例
-var cathy = new Dancer ('Cathy', 'Wong')
+var cathy = new Dancer('Cathy', 'Wong')
 cathy.greet() // Hi! Cathy Wong 這個方法繼承自Person類別
 cathy.dance() // Let's dance with Cathy Wong 這個方法繼承自Dancer類別
--- concepts/functions/callback.js
+++ concepts/functions/callback.js
@@ -10,26 +10,26 @@ function addUser (username) {
 
 function getUser () {
   setTimeout(function () {
-      console.log(users)
-    }, 100) // 比較早被執行
+    console.log(users)
+  }, 100) // 比較早被執行
 }
 
-addUser ('Jake')
-getUser () // ​​​​​[ 'Sam', 'Ellie', 'Bernie' ]​​​​​ 這邊會找不到Jake，因為addUser其實比較晚被執行
+addUser('Jake')
+getUser() // ​​​​​[ 'Sam', 'Ellie', 'Bernie' ]​​​​​ 這邊會找不到Jake，因為addUser其實比較晚被執行
 
 // 如果我們改用callback來寫，讓addUser的動作被執行完之後，才去getUser
 function addUser2 (username, callback) { // 傳入一個callback
-    setTimeout(function () {
-      users.push(username)
-      callback() // 而那個callback在完成新增使用者後，才被執行
-    }, 200)
-  }
-  
-  function getUser2 () {
-    setTimeout(function () {
-        console.log(users)
-      }, 100)
-  }
+  setTimeout(function () {
+    users.push(username)
+    callback() // 而那個callback在完成新增使用者後，才被執行
+  }, 200)
+}
+
+function getUser2 () {
+  setTimeout(function () {
+    console.log(users)
+  }, 100)
+}
 
 // Callback函式比較好的理解是一個「被當作參數傳遞，並在另一個函式中被執行的函式」
-addUser2('Howard', getUser2) // ​​​​​[ 'Sam', 'Ellie', 'Bernie', 'Jake', 'Howard' ]​​​​​
+addUser2('Howard', getUser2) // ​​​​​[ 'Sam', 'Ellie', 'Bernie', 'Jake', 'Howard' ]
