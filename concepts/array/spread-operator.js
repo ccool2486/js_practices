43ae7525fdd2c5e970274a822900541dbba88a01
Howard Huang <ccool2486@gmail.com>
2018-01-10 18:27:46 +0800

    spread & rest

---
+++ concepts/array/spread-operator.js
@@ -0,0 +1,25 @@
+// ES6提供了spread運算子
+// 可以幫助我們很快速的把陣列拆成多個值
+
+console.log([1, 2, 3]) // [1, 2, 3]
+
+console.log(...[1, 2, 3]) // 1 2 3
+
+let first = [1, 2, 3]
+let second = [4, 5, 6]
+
+first.push(second)
+console.log(first) // [1, 2, 3, [4, 5, 6]] 不是我們想要的，因為second整個被push進去了
+
+// 我們可以利用spread把上面的狀況解除
+first.push(...second) // 變成一個一個值塞進去
+console.log(first) // [1, 2, 3, 4, 5, 6]
+
+// This works for pushing in things for parameters:
+function addThreeThings (a, b, c) {
+  let result = a + b + c
+  console.log(result)
+}
+
+addThreeThings(...first)  // 6 ，把陣列中的「值」傳進去，前三個當作a, b, c，其他省略
+addThreeThings(...second)  // 15 ，把陣列中的「值」傳進去，前三個當作a, b, c，其他省略
---
+++ concepts/functions/rest-function-parameters.js
@@ -0,0 +1,46 @@
+// ES6 支援動態的參數數量
+// 可以讓所有傳進去function的參數，被當作一個陣列做處理
+
+// Before ES6 
+function Store () {
+  var aisle = {
+    fruit: [],
+    vegetable: []
+  }
+  return {
+    add: function (category) {
+          // 以前要把參數先變成陣列
+      var items = [].splice.call(arguments, 1)
+          //console.log(items);
+      items.forEach(function (value, index, array) {
+        aisle[category].push(value)
+      })
+    },
+    aisle: aisle
+  }
+}
+
+var myGroceryStore = new Store()
+myGroceryStore.add('fruit', 'apples', 'oranges')
+console.log(myGroceryStore.aisle) // ​​​​​{ fruit: [ 'apples', 'oranges' ], vegetable: [] }​​​​​
+
+// ES6
+function Store () {
+  var aisle = {
+    fruit: [],
+    vegetable: []
+  }
+  return {
+      // 現在只要用...item，就可以把後面所有的參數都當成陣列一員使用了
+    add: function (category, ...items) {
+      items.forEach(function (value, index, array) {
+        aisle[category].push(value)
+      })
+    },
+    aisle: aisle
+  }
+}
+
+var myGroceryStore = new Store()
+myGroceryStore.add('fruit', 'pears', 'peaches')
+console.log(myGroceryStore.aisle) // ​​​​​{ fruit: [ 'pears', 'peaches' ], vegetable: [] }​​​​​
