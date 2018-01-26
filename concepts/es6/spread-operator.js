2bcedb8352a40a3f3c3fc08897e4bd690d252c7d
Howard Huang <ccool2486@gmail.com>
2018-01-26 18:59:51 +0800

    Object Spread

--- concepts/es6/spread-operator.js
+++ concepts/es6/spread-operator.js
@@ -21,5 +21,20 @@ function addThreeThings (a, b, c) {
   console.log(result)
 }
 
+// 拿來取代concat蠻好用的
+let numbers = [1, 2, 3]
+const newNumbers = [...numbers, 4] // [1, 2, 3, 4]
+
 addThreeThings(...first)  // 6 ，把陣列中的「值」傳進去，前三個當作a, b, c，其他省略
 addThreeThings(...second)  // 15 ，把陣列中的「值」傳進去，前三個當作a, b, c，其他省略
+
+
+// 針對物件的spread應用，下面可能需要babel: https://babeljs.io/docs/plugins/transform-object-rest-spread/
+var obj1 = { foo: 'bar', x: 42 };
+var obj2 = { foo: 'baz', y: 13 };
+
+var clonedObj = { ...obj1 };
+// Object { foo: "bar", x: 42 }
+
+var mergedObj = { ...obj1, ...obj2 };
+// Object { foo: "baz", x: 42, y: 13 }
