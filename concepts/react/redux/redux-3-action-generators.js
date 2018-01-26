a7bd910904541768a1225dca7f29b830a6ad7c99
Howard Huang <ccool2486@gmail.com>
2018-01-26 16:05:41 +0800

    redux整理

---
+++ concepts/react/redux/redux-3-action-generators.js
@@ -0,0 +1,75 @@
+import { createStore } from 'redux';
+
+/**
+ * ##### 這個寫法是基本的action寫法 #####
+ *  store.dispatch({
+ *    type: 'INCREMENT',
+ *    incrementBy: 5
+ *  })
+ * 
+ * 
+ * ##### 但我們可以改用Action generator改寫 #####
+ * - 透過Function去產生action
+ * - 可以防止打錯字
+ * - 可以不把預設值寫在reducer裡面
+ * - ??? 為什麼要用destructuring? 用一般的function不好嗎？？
+ */
+
+
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy
+});
+
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
+});
+
+const resetCount = () => ({
+  type: 'RESET'
+});
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.incrementBy
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - action.decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
+store.dispatch(incrementCount());
+
+store.dispatch(resetCount());
+
+store.dispatch(decrementCount());
+
+store.dispatch(decrementCount({ decrementBy: 10 }));
+
+store.dispatch(setCount({ count: -100 }));
