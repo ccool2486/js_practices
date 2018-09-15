42682b91747d46aaf0f4a9a0bfea21630eae5654
黃軍皓 <ccool2486@gmail.com>
2018-09-15 07:29:37 +0000

    Reverse Words

--- concepts/algorithms/reverseWords.js
+++ concepts/algorithms/reverseWords.js
@@ -1,15 +1,25 @@
 /**
  * Udemy: https://www.udemy.com/learning-algorithms-in-javascript-from-scratch/learn/v4/t/lecture/7707446?start=0
- * 需求： 把字翻轉
+ * 需求：把字翻轉，但不是整句反轉，而是英文中的每個word反轉
+ * 需求: 不能用array.reverse()
+ * 
  */
 
-function reverseWords(str) {
+function reverseWords(str){
     var stringReversed = ''
+    var wordsArr = str.split(" ")
+    var wordReversedArr = wordsArr.map(reverseCharInWord)
+    stringReversed = wordReversedArr.join(' ')
+    return stringReversed
+    
+}
+
+function reverseCharInWord(str) {
+    let stringReversed = ''
     for (var i = str.length-1; i >= 0; i--){
-        stringReversed += (str[i]) //?
+        stringReversed += (str[i]) 
     }
     return stringReversed
 }
 
-
-reverseWords("Hello 你好嗎") //?
+console.log(reverseWords("Hello 你好嗎 ABCD"));
